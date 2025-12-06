b49a7546524580f57375dbb210d8a75bb1567c32
sabdelmalek-coder <said.abdelmalek@ail-tea.fr>
2025-12-06 15:05:33 +0100

    fix ip

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "blog_sg" {
   vpc_id =  data.aws_vpc.default.id
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks =  ["4.212.93.221"]
+  ingress_cidr_blocks =  ["4.212.93.221/32"]
 
   egress_rules = ["all-all"]
   egress_cidr_blocks =  ["0.0.0.0/0"]
