0f5fcbd43cd7a12806c7bc6cd0d07c04e24f081a
sabdelmalek-coder <said.abdelmalek@ail-tea.fr>
2025-12-07 12:57:55 +0100

    load balancer

--- main.tf
+++ main.tf
@@ -14,9 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -33,7 +31,6 @@ module "blog_vpc" {
   }
 }
 
-
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
@@ -41,12 +38,50 @@ resource "aws_instance" "blog" {
   
   subnet_id = module.blog_vpc.public_subnets[0]
 
- 
   tags = {
     Name = "HelloWorld"
   }
 }
 
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+  version = "10.3.1"
+  name    = "blog-alb"
+  
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  target_groups = [
+   {
+      name_prefix        = "blog-"
+      backend_protocol   = "HTTP"
+      backend_port       = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id        = aws_instance.blog.id
+          port = 80
+        }
+    }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ] 
+
+  tag = {
+    Environment = "dev"
+  }
+}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
